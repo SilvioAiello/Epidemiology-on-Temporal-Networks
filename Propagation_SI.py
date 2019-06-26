abb06b2b067189d3b4ba5f8e1383a0c2134912fb
SilvioAiello <silvio.aiello@studio.unibo.it>
2019-06-26 19:11:27 +0200

    Update Propagation_SI.py
    Some deep changes in structure of file, with a table of content.
    Also, now in DAR the epidemic starts from just one node

--- Propagation_SI.py
+++ Propagation_SI.py
@@ -9,34 +9,67 @@ import numpy as np #Used for its random functions and data structures
 import networkx as nx #Used to extract information from networks, and plot them
 import matplotlib.pyplot as plt #Used for graphic belluries
 
-### PARAMETERS OF THE SYSTEM
+#TODO: Spiegare
+
+# TABLE OF CONTENTS: after initial arrangements, two sections develop and perform SI propagations, and two sections develop and perform centrality measures.
+# Final section makes comparisons and prints result. Note that centrality measures are indipendent of the desease, and may be performed before its propagation.
+# 1) Set of parameters and network import
+# 2) Containers and functions for epidemic
+# 3) Epidemic propagation
+# 4) Containers and functions for centrality measures
+# 5) Centrality measures
+# 6) Comparisons and results print
+
+###                     PARAMETERS OF THE SYSTEM & NETWORKS IMPORT         ###
+
+#Here you can choose number of nodes, evolution length, number of iterations, DAR type, beta
 N = 100 #number of nodes of the network
 T = 100 #number of steps of temporal evolution
-K = 100 #number of repetitions 
+K = 50 #number of repetitions 
 P = 1 #DAR(P)
+beta = 0.15 #infection probability
 fig_count = 0 #several figures will be printed
 
 
-### NETWORKS IMPORT
 temporal_dar = np.loadtxt('Examples/DAR'+'%i_N%i_wholenetwork_T%i.txt' %(P,N,T))
 temporal_dar = temporal_dar.reshape((T,N,N)) #salvatolo come unica colonna, confermo che vuole tempo righe colonne
-temporal__fitn= np.loadtxt('Examples/FITN'+'_N%i_wholenetwork_T%i.txt' %(N,T))
-temporal__fitn= temporal__fitn.reshape((T,N,N))
+temporal_fitn= np.loadtxt('Examples/FITN'+'_N%i_wholenetwork_T%i.txt' %(N,T))
+temporal_fitn= temporal_fitn.reshape((T,N,N))
 
 
-### EPIDEMIC FUNCTIONS BUILDING
+###                      CONTAINERS AND FUNCTIONS FOR EPIDEMIC           ###
+
+# CONTAINERS #
+#The following (K,T,N) matrices will keep track of node-state evolution in time, so the entry kij means "state of node j, at time i and for the k-th iteration":
+#Quindi devi anticipare, da qualche parte sopra, che si itererà tutto K volte
+label_dar = np.zeros((T,N)) #infective state for DAR-network
+label_fitn = np.zeros((T,N))#infective state for FITN-network
+
+#This array saves the number of time steps needed to infect 60% of the network, for each of K iterations, for each node:
+score_dar = np.zeros((N,K))
+score_fitn = np.zeros((N,K))
+#The average result, for each node, is stored in a proper array
+avg_score_dar = np.zeros(N)
+avg_score_fitn = np.zeros(N)
+#This one will rank the nodes according to their scores. So, he will be involved in the comparison with the centralities:
+ranked_nodes_dar = np.zeros(N)
+ranked_nodes_fitn = np.zeros(N)
+
+
+# EPIDEMIC FUNCTIONS BUILDING #
 #The idea of SI model is that one infected node, at t-1, can infect one of its neighboroughs with probability beta
-beta = 0.15
+#The infection, appens as in Chen -> SPIEGARE
 
-### LAVORI IN CORSO
 from scipy.integrate import quad
 def poisson_probability(t):
+    #This function does.......
     lam = -np.log(1-beta) #applico la formula del Chen (pag 17), ma invece di 60 faccio direttamente 1
     return(lam*np.exp(-lam*t))
 quad(poisson_probability,0,np.inf)   
 
-
 def infect_extraction(t):
+    # Potrei farla più garbata definendo status, così ritorna un solo valore
+    #This functions does.....
     I = quad(poisson_probability,0,t)
     x = np.random.uniform(0,1) #soglia di probabilità da superare
     if I[0] > x:
@@ -44,36 +77,26 @@ def infect_extraction(t):
     else:
         return False
 
-
-### FINE LAVORI IN CORSO
-
-
 #Since the spread will be performed two times, on two different network, a function can be useful:
 #This function takes the neighboroughs of all infected nodes, and infects them with probability beta
 #It also updates the number of infections caused by that node
-def propagation(adiacency,state,killed,t):
+def propagation(adiacency,state,t):
     nextstate = np.zeros(N)
     for i in range(N):
         if state[i]==1:
             nextstate[i] = 1 #next state-vector is state be initially equal to the previous, so who is infected will stay infected
     graph = nx.convert_matrix.from_numpy_matrix(adiacency) #make adiacency a nx-graph, so finding neighboroughs is straightforward
     infected = [z for z in range(N) if state[z]==1]
-    #print(infected)
     for i in infected: #for each infected node...
         neighb = list(nx.neighbors(graph, i)) #...take its neighboroughs and put them in a list.
-        #print("Nodo infetto # %i, i suoi vicini sono " %i + str(neighb))
         for n in neighb: # Then, for each neighb...
             if nextstate[n] == 0: #...if it is (still!) susceptible...
                 if infect_extraction(t) == True: #...and the extraction is successful...
-                    nextstate[n] = 1 #...infect the node...
+                    nextstate[n] = 1 #...infect the node
                     #print("New infection! %i infected by %i" %(n,i))
-                    killed[i]+=1 #...and update the infecting score of the i-th node
-    #print("Giro concluso, nuovo label state:")
-    #print(nextstate)
-    #print("")
-    return(nextstate,killed)
+    return(nextstate)
 
-#It may be useful a function that counts the number of infected nodes at a certain time:
+#This function counts the number of infected nodes at a certain time:
 def infected_counter(infected):
     assert len(infected)==N, "Error: array has not length N"
     assert sum(infected)<=N, "Error: there are more infections then the actual number of nodes"
@@ -83,51 +106,111 @@ def infected_counter(infected):
             counter+=1
     return counter
 
-### EPIDEMIOLOGY ON DAR NETWORK:
-#The following (T,N)matrices will keep track of node-labels evolution in time, so the entry ij means "state, at time i, of node j":
-label_dar = np.zeros((T,N))
-label_fitn = np.zeros((T,N))
-#They are both initialized randomly, according to beta:
-label_dar[0]=np.random.choice((0,1),p=(1-beta,beta), size=N)
-
-label_fitn[0]=np.random.choice((0,1),p=(1-beta,beta), size=N)
-#Infective score is also created now; it will be used later to make comparisons with structural measures:
-infect_score_dar = np.zeros(N)
-infect_score_fitn = np.zeros(N)
-#Actual propagation:
-for t in range(T-1): #updating label state for both networks, providing the previous state
-#    print("t = %i" %t)
-#    print("Stato infettivo:")
-#    print(label_dar[t])
-    label_dar[t+1],infect_score_dar =propagation(temporal_dar[t],label_dar[t],infect_score_dar,t)
-    label_fitn[t+1],infect_score_fitn=propagation(temporal__fitn[t],label_fitn[t],infect_score_fitn,t)
-#Normalization of infection scores, so each i-th entry is the percentage of the network infected by the i-th node:
-infect_score_dar = 100*infect_score_dar/N
-infect_score_fitn = 100*infect_score_fitn/N
-#Using flip sort (L'HO SPIEGATA PIU AVANTI) we obtain a list of most infective nodes
-rankings_infections_dar = np.flip(np.argsort(infect_score_dar))
-rankings_infections_fitn = np.flip(np.argsort(infect_score_fitn))
+#Next function returns the time step at wich the disease has reached 60% of the network, if it exist:
+def score_60(scores):
+    time_60 = T-1 #initialized as the final temporal step
+    for t in range(T):
+        if infected_counter(scores)>=0.6*N:
+            time_60 = t
+            break
+    return time_60
 
 
-### PRINTS
-print("Epidemiological results, DAR:")
-print("Infection rate at 0: %.f %%" %(100*infected_counter(label_dar[0])/N))
-print("Infection rate at T/2: %.f %%" %(100*infected_counter(label_dar[int(T/2)])/N))
-print("Infection rate at T: %.f %%" %(100*infected_counter(label_dar[T-1])/N))
-print("Best 10 infecting node:")
-print(rankings_infections_dar[0:10])
-print("and their percentages:")
-print(infect_score_dar[rankings_infections_dar[0:10]])
+###                         PROPAGATIONS                         ###
+#Here the actual propagation occurs. First, the initial states are set for both networks, then propagation-function is called
+#Rembember that, while for DAR there's only 1 initial infected, there will be more for FITN.
+
+# DAR #
+for i in range(N):
+    initial_state = np.zeros(N) 
+    initial_state[0] = i #node i infected is the "index case" (in italian, "paziente zero")
+    #Now, everything is ready to perform propagation, K times
+    for k in range(K):
+        label_dar[0] =initial_state
+        for t in range(1,T): 
+            label_dar[t] =propagation(temporal_dar[t-1],label_dar[t-1],t-1)
+        score_dar[i,k] = score_60(label_dar[t]) #score updating
+    avg_score_dar[i] = np.mean(score_dar[i])
+
+#Ranking computation:
+# It's used a combination of two numpy functions:
+#Argsort, which takes a vector and returns a vector of input vector indices, in increasing order. So, here, the first entry would be the worst-scoring node.
+#Flip, which reverses a vector. So, after this, the first entry is the best-scoring node.
+ranked_nodes_dar = np.flip(np.argsort(avg_score_dar))
+
+
+
+# FITN #
+#Working progress
+
+
+###                      CONTAINERS AND FUNCTIONS FOR CENTRALITIES      ###
+
+# COMMUNICABILITY #
+#Building of Communicability matrix, just by applying definition and using some useful np functions:
+def communicability(temporal_): 
+    #As known, to compute communicability one as to choose a coefficient that multiplicates adiacencies
+    #At the moment, this function takes as default, as coefficient, a quarter of the inverse of max spectral radius
+    #Find max spectral radius:
+    spec = []
+    for t in range(T):
+        spec.append(np.real(max(np.linalg.eigvals(temporal_[t])))) #find max eigenvalue for each adiacency, taking only RE
+    maxradius = 1/max(spec) #maximum is the reciprocal of the maximum eigenvalue
+    #Communicability builing
+    Q = np.identity(N)/np.linalg.norm(np.identity(N))
+    for t in range(T):
+        inv = np.linalg.inv(np.identity(N)-0.25*maxradius*temporal_[t]) #inverse factor, which has to be multiplicated to the previous Q
+        Q = np.matmul(Q,inv)/np.linalg.norm(np.matmul(Q,inv)) #updating and normalizing of Q
+    return(maxradius,Q) #just for sake of completeness, also the max spectral radius is returned
+
+#Next two functions compute broadcast/receiving centralities and node rankings
+#For centralities, they use function np.sum, where one can choose to sum of lines (BC) or columns (RC)
+#For rankings, they use np.argsort, whose input is a list and output is a list of the indices of the input, sorted according to their decreasing values
+#So, the first element of the output list has the highest rank
+def broadcast_ranking(Q):
+    #Broadcast = sum over lines:
+    lines_sum = np.sum(Q, axis = 1) #this is a vector which reports the BC for each node
+    #Using argsort (flipping the result) to get the ranks vector: rank_i means the i-th best node
+    rank = np.flip(np.argsort(lines_sum)) #argsort returns the increasing order
+    return(lines_sum,rank)
+def receive_ranking(Q):
+    #Receive = sum over columns:
+    lines_sum = np.sum(Q, axis = 0) #this is a vector which reports the RC for each node
+    #Using argsort (flipping the result) to get the ranks vector: rank_i means the i-th best node
+    rank = np.flip(np.argsort(lines_sum)) #argsort returns the increasing order
+    return(lines_sum,rank)
+
+###                      CENTRALITIES MEASURES                      ###
+# DAR #
+spec_radius_dar, Q_dar = communicability(temporal_dar)
+nodes_Bcentrality_dar, nodes_Brank_dar = broadcast_ranking(Q_dar) #scores, node rankings
+nodes_Rcentrality_dar, nodes_Rrank_dar = receive_ranking(Q_dar) #cores, node rankings
+
+# FITN #
+spec_radius_fitn, Q_fitn = communicability(temporal_fitn)
+nodes_Bcentrality_fitn, nodes_Brank_fitn = broadcast_ranking(Q_fitn)
+nodes_Rcentrality_fitn, nodes_Rrank_fitn = receive_ranking(Q_fitn)
+
+
+
+###                     RESULTS PRINT                               ###
+print("###   DAR NETWORK   ###")
+print("Top 10- infective nodes, and their scores:")
+for i in range(10):
+    print(ranked_nodes_dar[i], avg_score_dar[ranked_nodes_dar[i]])
+print("Top 10-ranked Broadcast centrality, and their scores:")
+for i in range(10):
+    print(nodes_Brank_dar[i], nodes_Bcentrality_dar[nodes_Brank_dar[i]])
+print("Common nodes between infective and BC:")
+#Function intesection shows the common top-ranking nodes, but lists have to be converted in sets
+print(list(set(ranked_nodes_dar[0:9]).intersection(set(nodes_Brank_dar[0:9])))) 
+
 print("")
 print("")
-print("Epidemiological results, FITN:")
-print("Infection rate at 0: %.f %%" %(100*infected_counter(label_fitn[0])/N))
-print("Infection rate at T/2: %.f %%" %(100*infected_counter(label_fitn[int(T/2)])/N))
-print("Infection rate at T: %.f %%" %(100*infected_counter(label_fitn[T-1])/N))
-print("Best 10 infecting node:")
-print(rankings_infections_fitn[0:10])
-print("and their percentages:")
-print(infect_score_fitn[rankings_infections_fitn[0:10]])
+
+#For FITN it will be the same
+
+
 ### PLOTS
 ##To get some "readable" graphics, one can tell nx to show in red infected nodes, and in blue the others
 ##So, here is a functions that thakes the vector of state at time t and return a sequence of colours
@@ -168,60 +251,3 @@ print(infect_score_fitn[rankings_infections_fitn[0:10]])
 #np.savetxt(start_name+'_LABELS_DAR1_N%i_T%i.txt' %(N,T), label_dar) #saving labels in a separate file
 #np.savetxt(start_name+'FITN_N%i_T%i.txt' %(N,T), temporal__fitn.reshape(T,N*N))
 #np.savetxt(start_name+'_LABELS_FITN_N%i_T%i.txt' %(N,T), label_fitn) #saving labels in a separate file
-
-
-### CENTRALITY MEASURES
-    
-#Communicability analysis
-#Building of Communicability matrix, just by applying definition and using some useful np functions:
-def communicability(temporal_): 
-    #As known, to compute communicability one as to choose a coefficient that multiplicates adiacencies
-    #At the moment, this function takes as default, as coefficient, a quarter of the inverse of max spectral radius
-    #Find max spectral radius:
-    spec = []
-    for t in range(T):
-        spec.append(np.real(max(np.linalg.eigvals(temporal_[t])))) #find max eigenvalue for each adiacency, taking only RE
-    maxradius = 1/max(spec) #maximum is the reciprocal of the maximum eigenvalue
-    #Communicability builing
-    Q = np.identity(N)/np.linalg.norm(np.identity(N))
-    for t in range(T):
-        inv = np.linalg.inv(np.identity(N)-0.25*maxradius*temporal_[t]) #inverse factor, which has to be multiplicated to the previous Q
-        Q = np.matmul(Q,inv)/np.linalg.norm(np.matmul(Q,inv)) #updating and normalizing of Q
-    return(maxradius,Q) #just for sake of completeness, also tha max spectral radius is returned
-
-#Next two functions compute broadcast/receiving centralities and node rankings
-#For centralities, they use function np.sum, where one can choose to sum of lines (BC) or columns (RC)
-#For rankings, they use np.argsort, whose input is a list and output is a list of the indices of the input, sorted according to their decreasing values
-#So, the first element of the output list has the highest rank
-def broadcast_ranking(Q):
-    #Broadcast = sum over lines:
-    lines_sum = np.sum(Q, axis = 1) #this is a vector which reports the BC for each node
-    #Using argsort (flipping the result) to get the ranks vector: rank_i means the i-th best node
-    rank = np.flip(np.argsort(lines_sum)) #argsort returns the increasing order
-    return(lines_sum,rank)
-def receive_ranking(Q):
-    #Receive = sum over columns:
-    lines_sum = np.sum(Q, axis = 0) #this is a vector which reports the RC for each node
-    #Using argsort (flipping the result) to get the ranks vector: rank_i means the i-th best node
-    rank = np.flip(np.argsort(lines_sum)) #argsort returns the increasing order
-    return(lines_sum,rank)
-
-#Everything is set to perform analysis over the imported netowrks:
-#DAR
-spec_radius_dar, Q_dar = communicability(temporal_dar)
-nodes_Bcentrality_dar, nodes_Brank_dar = broadcast_ranking(Q_dar) #scores, node rankings
-nodes_Rcentrality_dar, nodes_Rrank_dar = receive_ranking(Q_dar) #cores, node rankings
-print("Top 10-ranked Broadcast centrality, and their score [DAR]:")
-for i in range(10):
-    print(nodes_Brank_dar[i], nodes_Bcentrality_dar[nodes_Brank_dar[i]])
-print("Common nodes between infective and BC:")
-print(list(set(rankings_infections_fitn[0:10]).intersection(nodes_Brank_dar[0:10])))
-#FITN
-spec_radius_fitn, Q_fitn = communicability(temporal__fitn)
-nodes_Bcentrality_fitn, nodes_Brank_fitn = broadcast_ranking(Q_fitn)
-nodes_Rcentrality_fitn, nodes_Rrank_fitn = receive_ranking(Q_fitn)
-print("Top 10-ranked Receive centrality, and their score [FITN]:")
-for i in range(10):
-    print(nodes_Brank_fitn[i], nodes_Bcentrality_fitn[nodes_Brank_fitn[i]])
-print("Common nodes between infective and BC:")
-print(list(set(rankings_infections_fitn[0:10]).intersection(nodes_Brank_fitn[0:10])))
