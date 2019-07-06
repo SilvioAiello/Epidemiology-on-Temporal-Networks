18a6a2364be5fa41d00bb7d6153c4b7647a394fe
SilvioAiello <silvio.aiello@studio.unibo.it>
2019-07-06 12:26:17 +0200

    Generalization of EVOLUTION_DAR

--- Evolution_DAR.py
+++ Evolution_DAR.py
@@ -7,14 +7,7 @@
 import numpy as np #Used for its random functions and data structures
 import networkx as nx #Used to extract information from networks, and plot them
 import matplotlib.pyplot as plt #Used for graphic belluries
-
-### PARAMETERS OF THE SYSTEM
-
-N = 100 #number of nodes of the network
-T = 100 #number of steps of temporal evolution
-K = 100 #number of repetitions 
-P = 1 #DAR(P)
-fig_count = 0 #several figures will be printed
+import pickle
 
 ### EVOLUTION
 
@@ -29,49 +22,105 @@ fig_count = 0 #several figures will be printed
 #Otherwise, the new state will be extacted by a "coin toss", with probability xi_ij of getting "1"
 #The evolution is performed just for the upper triangular adiancency, and than the matrix is simmetrized
 
-#At the moment, alpha will be the same for all entries; alpha = 0.66 and xi = 0.5:
-alpha = 0.66*np.ones((N,N))
-xi = 0.5*np.ones((N,N))
 
-#Initial state is generated randomly
-#np.random.seed(1)
-initial_state = [[np.random.choice((0,1),p=(0.5,1-0.5)) if j>i else 0 for j in range(N)] for i in range(N)]
-initial_state = [[initial_state[j][i] if j<i else initial_state[i][j] for j in range(N)] for i in range(N)]
+def network_generation_dar(N,T,alpha,xi, P=1):
+    #potresti aggiungere una variabile indicatrice tipo undirected = True
+    #Genera un intero network di evoluzione
+    #Initial state is generated randomly
+    initial_state = [[np.random.choice((0,1),p=(0.5,1-0.5)) if j>i else 0 for j in range(N)] for i in range(N)]
+    initial_state = [[initial_state[j][i] if j<i else initial_state[i][j] for j in range(N)] for i in range(N)]
+    #Tensor generation, T=0 equal to initial_state
+    temporal_network = np.zeros((T,N,N))
+    temporal_network[0] = initial_state
+    #Network update
+    #First comprehension says: for each row i, taking only the upper triangle (j>i), choose the value 
+    #from the same cell of the T-P matrix, with probability alpha, or from a coin tossing (whose probability of getting 1 is xi).
+    #The second comprehension just simmetrizes the matrix
+    for t in range(1,T):
+        temporal_network[t] = [[np.random.choice((temporal_network[t-P,i,j],np.random.choice((1,0), p=(xi[i,j],1-xi[i,j]))),p=(alpha[i,j],1-alpha[i,j])) if j>i else 0 for j in range(N)] for i in range(N)]
+        temporal_network[t] = [[temporal_network[t,j,i] if j<i else temporal_network[t,i,j] for j in range(N)] for i in range(N)]
+    return temporal_network
 
-#Tensor generation, T=0 equal to initial_state
-temporal_network = np.zeros((T,N,N))
-temporal_network[0] = initial_state
+# NETWORK ANALYSYS #
+def degree_node(network,node,out = True):
+    #you give an adiacency for a certain t, and a node: this function computes node degree
+    #if out = true, sums the line (link that start from node)
+    #if out = false, sums the column (link that reach that node)
+    #if graph is undirected, out = in
+    if out:
+        return sum(network[node])
+    else:
+        return sum(network[:,node])
 
-#Network update
-#First comprehension says: for each row i, taking only the upper triangle (j>i), choose the value 
-#from the same cell of the T-P matrix, with probability alpha, or from a coin tossing (whose probability of getting 1 is xi).
-#The second comprehension just simmetrizes the matrix
-for t in range(1,T):
-    temporal_network[t] = [[np.random.choice((temporal_network[t-P,i,j],np.random.choice((1,0), p=(xi[i,j],1-xi[i,j]))),p=(alpha[i,j],1-alpha[i,j])) if j>i else 0 for j in range(N)] for i in range(N)]
-    temporal_network[t] = [[temporal_network[t,j,i] if j<i else temporal_network[t,i,j] for j in range(N)] for i in range(N)]
+def degree_mean_t(network,out = True):
+    #this function is not "pure", since it relies on another function
+    #network must be a N*N np.array
+    #you give an adiacency for a certain t: it computes mean degree at that t, using degree_node
+    #depending in out value, it returns out degree or in degree. If undirected, it's the same
+    degrees = []
+    for node in range(len(network)):
+        degrees.append(degree_node(network,node,out))
+    return(np.average(degrees)) #it returns a number
+
+def degree_mean_sequence(network,T,out = True):
+    #here you give the whole set of adiacencies
+    d_seq = []
+    for t in range(T):
+        d_seq.append(degree_mean_t(network[t],out))
+    return d_seq #it return a list
+    
+
+# NETWORK SAVE #
+
+def make_name(N,T,isDAR, start,P=1):
+    """ isDAR = True -> add "DAR(P); False -> add "TGRG"
+    start should be like a str like "alphaeqsxieqs"
+    """
+    if isDAR:
+        return "Networks/N"+str(N)+"_T"+str(T)+"_DAR"+str(P)+"_"+start
+    if isDAR == False:
+        return "Networks/N"+str(N)+"_T"+str(T)+"_TGRG_"+start
+
+def network_save(network, N,T,isDAR, start,P=1,k=1):
+    #Il motivo per cui lo metto qua è che questa funzione genera l'intero percorso dove storo tutto
+    name = make_name(N,T,isDAR,start,P)+"/realization"+str(k)+"/network.txt"
+    with open(name, 'wb') as handle:
+        return pickle.dump()
+
+# PLOT FUNCTIONS #
+def networkplot(graph,figcount,savefig=False, figname = None, k = None, t = None):
+    """Requires an adiacency[t] and the figure count
+    If you want to save the image, you have to say savefig=True, and pass a 2-ple and the value of k and t.
+    Tuple figname contains the values needed by make_name: N,T,isDAR, start,P (P MUST BE SPECIFIED HERE)"""
+    figcount+=1
+    plt.figure(figcount)
+    plt.title("")
+    nxgraph = nx.from_numpy_matrix(graph) #convert np-matrix in a Network, readable by nx library:
+    nx.draw(nxgraph, pos = nx.drawing.layout.circular_layout(nxgraph), with_labels = True) #draw a circular plot and show the number of each node
+    if savefig and figname and k and t:
+        plt.savefig(make_name(figname)+"Results"+'_realization%i_t=%i.pdf' %(k,t))
+    return figcount+1, plt.show()
 
 ### RESULTS PRINT AND SAVE:
 start_name = 'Examples/DAR' #begin of the name of files that will be saved (txt,img...)
 degree_evolution = [] #here the sequence of mean degrees will be held
-for t in range(T):
-    plt.figure(fig_count)
-    fig_count+=1
-    #Function "from_numpy_matrix" converts a np matrix in a Network, readable by nx library:
-    grafo = nx.from_numpy_matrix(temporal_network[t])
     #Since it's not possible to directly handle the mean degree, here it is computed:
     degree_evolution.append(sum([grafo.degree(i) for i in range(N)])/N)
-    #Function draw is provided of two informations: draw a circular plot and show the label (i.e. the number) of each node
-    #nx.draw(grafo, pos = nx.drawing.layout.circular_layout(grafo), with_labels = True)
+        #nx.draw(grafo, pos = nx.drawing.layout.circular_layout(grafo), with_labels = True)
     #plt.savefig(start_name+'%i_N%i_image_t=%i.pdf' %(P,N,t))
     #plt.show()
 print(degree_evolution)
 
 ### TESTS:
-for t in range(T):
-    assert (temporal_network[t] == temporal_network[t].T).any, "Error: network at t = %i is not symmetric" %t
-    assert sum(np.diag(temporal_network[t])) == 0, "Error: network at t = %i has not-0 diagonal" %t
+def test_symmetry(temporal_network,T):
+    for t in range(T):
+        assert (temporal_network[t] == temporal_network[t].T).any, "Error: network at t = %i is not symmetric" %t
+
+def test_nulldiagonal(temporal_network,T):
+        assert sum(np.diag(temporal_network[t])) == 0, "Error: network at t = %i has not-0 diagonal" %t
+
 #Save the network for further use
-np.savetxt(start_name+'%i_N%i_wholenetwork_T%i.txt' %(P,N,T), temporal_network.reshape(T*N*N,1))
+#np.savetxt(start_name+'%i_N%i_wholenetwork_T%i.txt' %(P,N,T), temporal_network.reshape(T*N*N,1))
 #To import:
 #new_data = np.loadtxt('start_name+'%i__N%i_wholenetwork_T%i.txt' %(P,N,T))
 #new_data = new_data.reshape((T,N,N))
