be7e8adc99267678dade644851a0fa1d75e7e7cc
SilvioAiello <silvio.aiello@studio.unibo.it>
2019-07-19 20:21:49 +0200

    Added prelimanry assertions

--- Propagation_SI.py
+++ Propagation_SI.py
@@ -12,14 +12,14 @@ For further understandings on how this script operates, check file "howto.md".
 For further theoretical understandings, check file "explanation.md".
 """
 import numpy as np #Used for its random functions and data structures
-import Test_suit
+import Test_suite
 #%% EASING FUNCTIONS
-def neighbourhood(adiacency,node):
+def neighbourhood(adjacency,node):
     """Extracts the neighbourhood reachable by a node a given time. 
     
     Parameters
     ----------
-    adiacency : np.array
+    adjacency : np.array
         Adiacency matrix (dar, tgrg...), N*N-array of values 0 or 1, expressing existence state of a link between two nodes at a certain time
         As an example, it may be a temporal_dar[t]
     node: int
@@ -30,7 +30,16 @@ def neighbourhood(adiacency,node):
     neigh: set
         Set of neighbours linked to the input node
     """
-    neigh = {i for i in range(len(adiacency)) if adiacency[node,i]==1}
+    #ASSERTS
+    Test_suite.assert_ndarray(adjacency,2)
+    Test_suite.assert_square(adjacency)
+    Test_suite.assert_nulldiagonal(adjacency)
+    
+    assert isinstance(node, int)
+    assert node <= len(adjacency), "Error: node not present"
+     
+    #FUNCTION
+    neigh = {i for i in range(len(adjacency)) if adjacency[node,i]==1}
     return neigh
 
 def onlyzeros(nodes_set,states_dict):
@@ -40,8 +49,8 @@ def onlyzeros(nodes_set,states_dict):
     ----------
     nodes_set: tuple/list/set
         Ensemble of nodes (not necessairly all network's nodes)
-    states_dict: dict
-        Dictionary mapping state of required nodes to a 0 or 1 value.
+    states_dict: N-dict
+        Dict mapping state of required nodes to a 0 or 1 value.
         It can have more keys than nodes_set: the extra ones won't be scanned.
         As an example, state_dict may be a label_dar[t]
     
@@ -50,21 +59,20 @@ def onlyzeros(nodes_set,states_dict):
     selected: set
         Set of susceptible nodes
     """
+    assert type(states_dict) == dict, "states_dict is not a dictionary"
+    
     selected = {node for node in nodes_set if states_dict[node]==0} #0 means susceptible
     return selected
 
-def contact_lasting(adjacencies,state,t,infected_node,susceptible_node):
+def contact_lasting(tempnet,states_sequence,t,infected_node,susceptible_node):
     """
-    Computes the number of consectuive temporal steps a couple of nodes I-S are linked, as long as they keep these states.
+    Computes the number of consectuive temporal steps a couple of nodes I-S are linked, as long as they hold these states.
     
     Parameters
     ----------
-    adjacencies: NTT-np.array
-        Whole temporal network
-    state: TN-dict
-        Whole sequence of nodes' states
-    t: int
-        starting time step (from which going backwards in time)
+    tempnet: TNN-np.array
+    states_sequence: TN-dict
+    t: int, time step from which going backwards in time
     infected_node, susceptible_node: int
     
     Returns
@@ -72,14 +80,31 @@ def contact_lasting(adjacencies,state,t,infected_node,susceptible_node):
     counter: int
         SI contact duration; if 0, nodes are not linked, if t, they have been linked since network is born
     """
-    #Assertions
-    assert state[t][infected_node] == 1, "Error: infected node is not infected at the latest instant"
-    assert state[t][susceptible_node] == 0, "Error: susceptible node is not susceptible at the latest instant"
-    assert adjacencies[t,infected_node,susceptible_node] == 1, "Error: nodes couple is not linked at latest instant"
-    #Function
+    #ASSERTS
+    assert isinstance(t, int)
+    assert isinstance(infected_node, int)
+    assert isinstance(susceptible_node, int)
+    assert isinstance(states_sequence, dict), "states_sequence is not a dictionary"
+    
+    T = tempnet.shape[0]
+    N = tempnet.shape[1]
+    assert t <= T, "t is bigger than the total network duration"
+    assert infected_node<N, "infected_node doesn't exist"
+    assert susceptible_node<N, "susceptible_node doesn't exist"
+    
+    Test_suite.assert_ndarray(tempnet,3) #ask it to be a square array of the first dimension of the network
+    [Test_suite.assert_square(tempnet[i]) for i in range(t)] #check square for each step
+    [Test_suite.assert_nulldiagonal(tempnet[i]) for i in range(t)] #check null diagonal for each step
+    
+    assert infected_node != susceptible_node, "Nodes are the same"
+    assert states_sequence[t][infected_node] == 1, "Infected node is not infected at the latest instant"
+    assert states_sequence[t][susceptible_node] == 0, "Susceptible node is not susceptible at the latest instant"
+    assert tempnet[t,infected_node,susceptible_node] == 1, "Nodes couple is not linked at latest instant"
+    
+    #FUNCTION
     counter = 0
     for instant in range(t+1):
-        if (adjacencies[t-instant,infected_node,susceptible_node] == 1 and state[t-instant][infected_node] != state[t-instant][susceptible_node]):
+        if (tempnet[t-instant,infected_node,susceptible_node] == 1 and states_sequence[t-instant][infected_node] != states_sequence[t-instant][susceptible_node]):
             counter +=1
         else:
             break
@@ -109,6 +134,18 @@ def propagation(tempnet,index_case,probabilities): #Remember: the outcome of thi
     T = tempnet.shape[0] #shape of array returns (T,N,N)-tuple
     N = tempnet.shape[1] #shape of array returns (T,N,N)-tuple
     
+    #ASSERTS
+    assert isinstance(index_case, int)
+    assert index_case<N, "index_case doesn't exist"
+    
+    Test_suite.assert_ndarray(tempnet,3) #ask it to be a square array of the first dimension of the network
+    [Test_suite.assert_square(tempnet[t]) for t in range(T)] #check square for each step
+    [Test_suite.assert_nulldiagonal(tempnet[t]) for t in range(T)] #check null diagonal for each step
+    
+    assert isinstance(probabilities, dict), "Probabilities is not a dictionary"
+    assert len(probabilities) == T, "Probabilities doesn't meet tempnet duration"
+    
+    #FUNCTION
     def set_infected(node,t): #once one is infected,it stays infeced
         for instant in range(t,T):
             states_sequence[instant][node] = 1
@@ -148,7 +185,7 @@ def infected_counter(set_of_nodes):
         if set_of_nodes[i]==1:
             counter+=1
     return counter
-def time_score(scores,fraction):
+def time_score(scores_evolution,fraction):
     """
     Returns the time each node spent to infect a fraction of network, for one whole iteration.
     
@@ -158,7 +195,7 @@ def time_score(scores,fraction):
     
     Parameters
     ----------
-    scores: dict of dicts
+    scores: TN-dict
         Sequence of T dictionaries of the states of all N nodes (T and N are extracted by function iteself)
     fraction: float
         Real number, from 0 to 1, representing network fraction
@@ -168,15 +205,18 @@ def time_score(scores,fraction):
     time_spent: int
         Score for that iteration
     """
-    #Scores sarebbe il label dar da 0 a T, e lui deve trovare in quale label_dar[t] la soglia è stata superata
-    T,N = len(scores),len(scores[0])
+    T = len(scores_evolution)
+    N = len(scores_evolution[0])
+    
+    #ASSERTS
     assert fraction > 0, "Error, only values between 0 and 1 are allowed"
     assert fraction < 1, "Error, only values between 0 and 1 are allowed"
-    #asserire che sum deve essere sempre <= N?
+    assert isinstance(scores_evolution, dict), "scores_evolution is not a dictionary"
+    
+    #FUNCTION
     time_spent = T-1 #initialized as the final temporal step
     for t in range(T):
-        if infected_counter(scores[t])>=fraction*N:
+        if infected_counter(scores_evolution[t])>=fraction*N:
             time_spent = t
             break
-    return time_spent 
-#TODO: ha senso restituire il total time? magari inf, magari errore, vediamo
+    return time_spent
