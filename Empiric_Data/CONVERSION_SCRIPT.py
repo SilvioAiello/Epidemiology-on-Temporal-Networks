c2e8de6f7a5ac32f1278359f8e3ed2739e64e718
SilvioAiello <silviuccio94@gmail.com>
2019-09-25 16:23:09 +0200

    name save change

--- Empiric_Data/CONVERSION_SCRIPT.py
+++ Empiric_Data/CONVERSION_SCRIPT.py
@@ -7,7 +7,7 @@ Created on Sat Sep 14 16:54:54 2019
 #%% GENERATING FILES
 import scipy.io
 
-mat = scipy.io.loadmat('C:/Users/Silvio/Documents/GitHub/Epidemiology-on-Temporal-Networks/Empiric_Data/parametri_stimati_EMID.mat')
+mat = scipy.io.loadmat('parametri_stimati_EMID.mat')
 
 alphaDAR1 = mat['alphaDAR1']
 chiDAR1 = mat['marginalProbabilityDAR1']
@@ -17,13 +17,13 @@ sigmaTGRG = mat['sigmaTGRG']
 
 import pickle
 
-with open('C:/Users/Silvio/Documents/GitHub/Epidemiology-on-Temporal-Networks/Empiric_Data/alphaDAR1.pkl', 'wb') as handle: #wb = write binary
+with open('alphaDAR1.pkl', 'wb') as handle: #wb = write binary
     pickle.dump(alphaDAR1,handle)
-with open('C:/Users/Silvio/Documents/GitHub/Epidemiology-on-Temporal-Networks/Empiric_Data/chiDAR1.pkl', 'wb') as handle: #wb = write binary
+with open('chiDAR1.pkl', 'wb') as handle: #wb = write binary
     pickle.dump(chiDAR1,handle)
-with open('C:/Users/Silvio/Documents/GitHub/Epidemiology-on-Temporal-Networks/Empiric_Data/phi0TGRG.pkl', 'wb') as handle: #wb = write binary
+with open('phi0TGRG.pkl', 'wb') as handle: #wb = write binary
     pickle.dump(phi0TGRG,handle)
-with open('C:/Users/Silvio/Documents/GitHub/Epidemiology-on-Temporal-Networks/Empiric_Data/phi1TGRG.pkl', 'wb') as handle: #wb = write binary
+with open('phi1TGRG.pkl', 'wb') as handle: #wb = write binary
     pickle.dump(phi1TGRG,handle)
-with open('C:/Users/Silvio/Documents/GitHub/Epidemiology-on-Temporal-Networks/Empiric_Data/sigmaTGRG.pkl', 'wb') as handle: #wb = write binary
+with open('sigmaTGRG.pkl', 'wb') as handle: #wb = write binary
     pickle.dump(sigmaTGRG,handle)
